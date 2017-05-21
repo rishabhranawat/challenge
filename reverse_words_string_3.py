ef29e17087baf0d369053317dd7e5956d136712c
rishabhranawat <rishabhranawat12345@gmail.com>
2017-05-21 10:30:13 +0530

    better solution

--- reverse_words_string_3.py
+++ reverse_words_string_3.py
@@ -1,6 +1,18 @@
-# Horrible Runtime.
-# Will getback to it in a bit.
+# Problem Source: LeetCode
 
+# Given a string, you need to reverse the order of characters in 
+# each word within a sentence while still 
+# preserving whitespace and initial word order.
+
+# Example 1:
+# Input: "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
+# Note: In the string, each word is separated by single space 
+# and there will not be any extra space in the string.
+###############################
+
+
+# O(n^2)
 def reverseWords(s):
     """
     :type s: str
@@ -8,7 +20,6 @@ def reverseWords(s):
     """
     ret_str = ""
     words = s.split(" ")
-
     for word in words:
         rev_word = ""
         for i in range(len(word)-1, -1, -1):
@@ -16,6 +27,18 @@ def reverseWords(s):
         ret_str += " "+rev_word
     return ret_str.strip()
 
-print(reverseWords("Let's take LeetCode contest").strip())
+# O(n)
+def reverseWords2(s):
+    ret_str = ""
+    word = ""
+    for letter in s:
+        if(letter == " "):
+            ret_str += " "+word
+            word = ""
+        else:
+            word = letter + word
+    return (ret_str+" "+word).strip()
+
+print(reverseWords2("Let's take LeetCode contest").strip())
 
             
