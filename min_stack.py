ab26aee545b5537b0426807dcf787766b5e4caec
rishabhranawat <rishabhranawat12345@gmail.com>
2017-07-15 01:39:43 -0400

    min stack works

--- min_stack.py
+++ min_stack.py
@@ -4,32 +4,49 @@ class MinStack(object):
         """
         initialize your data structure here.
         """
+        self.minElements = []
+        self.elements = []
+        self.currMin = None
 
     def push(self, x):
         """
         :type x: int
         :rtype: void
         """
-
+        self.elements.append(x)
+        if(x < self.currMin or self.currMin == None):
+            self.currMin = x
+            self.minElements.append(x)
+        else:
+            self.minElements.append(self.currMin)
         
 
     def pop(self):
         """
         :rtype: void
         """
-
+        self.elements.pop()
+        self.minElements.pop()
+        if(len(self.minElements) > 0):
+            self.currMin = self.minElements[-1]
+        else:
+            self.currMin = None
+        
 
     def top(self):
         """
         :rtype: int
         """
-
+        return self.elements[-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
+        return self.currMin
+
+
         
 
 
