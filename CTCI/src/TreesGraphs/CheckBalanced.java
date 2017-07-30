7e4224353e4e1981660b091e14f1e923b26881aa
rishabhranawat <rishabhranawat12345@gmail.com>
2017-07-30 12:17:21 -0400

    is balanced

--- CTCI/src/TreesGraphs/CheckBalanced.java
+++ CTCI/src/TreesGraphs/CheckBalanced.java
@@ -1,7 +1,50 @@
 package TreesGraphs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by rishabh on 30/07/17.
  */
 public class CheckBalanced {
+    public Integer getHeight(TreeNode root){
+        if(root == null) return 1;
+        return 1 + Integer.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public boolean checkBalanced(TreeNode root){
+        ListOfDepths l = new ListOfDepths();
+        List<List<TreeNode>> lds = l.listOfDepths(root);
+
+        for(List<TreeNode> level : lds){
+            Integer d = null;
+            for(TreeNode node: level){
+                Integer h = getHeight(node);
+                if(d == null) d = h;
+                else if(h - d > 1) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        TreeNode seven = new TreeNode(7);
+        TreeNode eight = new TreeNode(8);
+        TreeNode nine = new TreeNode(9);
+        TreeNode ten = new TreeNode(10);
+        TreeNode eleven = new TreeNode(11);
+        TreeNode twel = new TreeNode(12);
+        TreeNode thirt = new TreeNode(13);
+
+        thirt.left = eleven;
+        nine.left = twel;
+        nine.right = thirt;
+
+
+        seven.left = eight;
+        seven.right = nine;
+
+        CheckBalanced c = new CheckBalanced();
+        System.out.println(c.checkBalanced(seven));
+    }
 }
---
+++ leetcode/is_balanced.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def getH(self, head):
+        if(head == None): return 0
+        else:
+            return 1 + max(self.getH(head.right), self.getH(head.left))
+    
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if(root == None): return True
+        l = self.getH(root.left)
+        r = self.getH(root.right)
+        if(abs(l-r) > 1): return False
+        else: return self.isBalanced(root.left) and self.isBalanced(root.right)
+        
+        
